c5e71e5d794018298c3f49798b4c7708f5d818d4
Geraldo Galvao <gmgn@cin.ufpe.br>
2021-03-07 12:04:45 -0300

    Eureka server

---
+++ hr-eureka-server/src/main/java/com/gmgn/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.gmgn.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
