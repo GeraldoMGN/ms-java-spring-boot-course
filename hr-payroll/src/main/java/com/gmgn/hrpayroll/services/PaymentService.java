69c7fe099073c7d914a5c84496329715b426ec70
Geraldo Galvao <gmgn@cin.ufpe.br>
2021-03-07 10:45:00 -0300

    Implements hr-payroll microservice

---
+++ hr-payroll/src/main/java/com/gmgn/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.gmgn.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.gmgn.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
